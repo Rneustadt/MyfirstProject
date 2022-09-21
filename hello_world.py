b5479275547625d8b555b563b7e1f3e999d9dc9b
Rneustadt <neustadt.robert@yahoo.com>
2022-09-20 18:30:26 -0700

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+print ('Lets add numbers')
+print ('pick a number between one and ten')
+myFirstNumber = input ()
+print ('ok you picked' + myFirstNumber )
+print ('ok pick another number between one and ten' )
+mySecondNumber = input ()
+print ('ok you picked' + mySecondNumber)
+myFirstNumber = int(myFirstNumber)
+mySecondNumber = int(mySecondNumber)
+combinedNumber = myFirstNumber + mySecondNumber
+print ('your combined number is')
+print (combinedNumber)
