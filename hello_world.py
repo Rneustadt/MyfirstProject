fbab667e35eb9658f2d7e960437eceaa79c6d6fe
Rneustadt <neustadt.robert@yahoo.com>
2022-09-20 19:33:07 -0700

    added operations & cleaned up code

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,21 @@
 print ('Lets add numbers')     # this is a comment
-print ('pick a number between one and ten')
-myFirstNumber = input ()
-print ('ok you picked' + myFirstNumber )
-print ('ok pick another number between one and ten' )
-mySecondNumber = input ()
-print ('ok you picked' + mySecondNumber)
-myFirstNumber = int(myFirstNumber)
-mySecondNumber = int(mySecondNumber)
-combinedNumber = myFirstNumber + mySecondNumber
-print ('your combined number is')
-print (combinedNumber)
+myFirstNumber = int (input ('pick a number between one and ten - \n'))
+print ('ok you picked' + str(myFirstNumber ))
+mySecondNumber = int (input ('ok pick another number between one and ten - \n'))
+print ('ok you picked' + str(mySecondNumber))
+myselection = input('choose opperation')
+print ('you selected ' + myselection) 
+result = ''
+if myselection == '+':
+   result = int(myFirstNumber) + int(mySecondNumber)
+elif myselection =='-':
+    result = int(myFirstNumber) - int(mySecondNumber)
+elif myselection =='*':
+    result = int(myFirstNumber) * int(mySecondNumber)
+elif myselection =='/':
+    result = int(myFirstNumber) / int(mySecondNumber)
+elif myselection =='%':
+    result = int(myFirstNumber) % int(mySecondNumber)
+else:
+    print ('wrong selection')
+print (result)
